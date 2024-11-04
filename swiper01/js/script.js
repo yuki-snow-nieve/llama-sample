c106ec785704d9828ecf22c872df62a750b32fa9
yuki-snow-nieve <niever66@gmail.com>
2024-11-05 02:40:56 +0900

    vertical swiper type01 縦位置のズレ修正

--- swiper01/js/script.js
+++ swiper01/js/script.js
@@ -1,5 +1,9 @@
 document.addEventListener( 'DOMContentLoaded', () => {
-  const movieDetailBox = document.getElementById('box-detail')
+  const movieDetailBox = document.getElementById('box-detail');
+  const toggleButton_showDetail = document.getElementById('button-showDetail_toggle');
+  const button_hideDetail = document.getElementById('button-hideDetail');
+  const areaDetail = document.getElementById('area_detail');
+  let flg_showDetail = 0;
 
   const swiper = new Swiper('.swiper', {
     direction: 'vertical',
@@ -7,6 +11,7 @@ document.addEventListener( 'DOMContentLoaded', () => {
     initialSlide: 1,
     slidesPerView: 'auto',
     effect: 'slide',
+    speed: 400,
     // history: {
     //   replaceState: true,
     //   key: ''
@@ -16,13 +21,66 @@ document.addEventListener( 'DOMContentLoaded', () => {
     centeredSlidesBounds: true,
     lazyPreloadPrevNext: 2,
     on: {
-      slideChangeTransitionStart: () => {
+      slideNextTransitionStart: (e) => {
+      },
+      slidePrevTransitionStart: (e) => {
+      },
+      slideChangeTransitionStart: (e) => {
+        deletePlayerItem(e.slides[e.snapIndex]);
         movieDetailBox.style.opacity = 0;
       },
-      slideChangeTransitionEnd: () => {
+      slideChangeTransitionEnd: (e) => {
+        startAutoPlay(e.slides[e.snapIndex]);
         movieDetailBox.style.opacity = 1;
       }
     }
   });
+
+  function startAutoPlay(item) {
+    const player = document.createElement('iframe');
+    player.setAttribute('width', '100%');
+    player.setAttribute('height', '100%');
+    player.setAttribute('src', item.dataset.movie + '&autoplay=1');
+    player.setAttribute('title', '');
+    player.setAttribute('frameborder', 0);
+    player.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+    player.setAttribute('referrerpolicy', 'strict-origin-when-cross-origin');
+    player.setAttribute('allowfullscreen', '');
+    item.insertAdjacentElement('beforeend', player);
+  };
+
+  function deletePlayerItem(item) {
+    if (!item.querySelector('iframe')) {
+      return;
+    }
+    item.querySelector('iframe').remove();
+  }
+
+  toggleButton_showDetail.addEventListener('click', () => {
+    if (flg_showDetail == 0) {
+      openDetail();
+    } else {
+      closeDetail();
+    }
+  });
+
+  button_hideDetail.addEventListener('click', () => {
+    closeDetail();
+  })
+
+  function openDetail() {
+    flg_showDetail = 1;
+    areaDetail.classList.add('is_showmore');
+    toggleButton_showDetail.textContent = '詳細を縮小する';
+    button_hideDetail.style.display = 'block';
+  }
+
+  function closeDetail() {
+    flg_showDetail = 0;
+    areaDetail.classList.remove('is_showmore');
+    toggleButton_showDetail.textContent = '詳細をもっと見る';
+    button_hideDetail.style.display = 'none';
+  }
+
 
 });
