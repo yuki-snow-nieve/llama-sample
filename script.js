ba3bc07e34e6eae9ccc4cca58c43163c5bec265a
yuki-snow-nieve <niever66@gmail.com>
2024-11-12 03:43:12 +0900

    サンプル情報修正

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener( 'DOMContentLoaded', () => {
-  let w = window.innerWidth;
-  let h = window.innerHeight;
-
   const dispW = document.getElementById('windowW');
   const dispH = document.getElementById('windowH');
 
-  dispW.innerText = w + 'px'
-  dispH.innerText = h + 'px'
+  getWindowSize();
+
+  window.addEventListener("resize", getWindowSize);
+
+  function getWindowSize() {
+    let w = window.outerWidth;
+    let h = window.outerHeight;
+
+    dispW.innerText = w + 'px';
+    dispH.innerText = h + 'px';
+  
+  }
 })
