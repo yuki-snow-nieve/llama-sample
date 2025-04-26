7e861e32881f9eac6fd79a979fd40640615e6e69
yuki-snow-nieve <niever66@gmail.com>
2025-04-27 01:32:48 +0900

    netlify vue spa router対策

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -6,8 +6,8 @@ import PlayerView from '../views/PlayerView.vue';
 import PlayerItemView from '../views/PlayerItemView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
   mode: 'history',
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
