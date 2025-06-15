bc1f8e93637fb5212df454f0fcef7844d43d8e7c
yuki-snow-nieve <niever66@gmail.com>
2025-06-16 01:44:08 +0900

    transition書き換え

--- vue-project/src/views/temp.vue
+++ vue-project/src/views/temp.vue
@@ -18,85 +18,87 @@ onBeforeRouteLeave((to, from, next) => {
 //transitionの設定
 const transition_toPlayer = (to, from, next) => {
   const transitionDuration = 1400;
-
-  const active_index = Array.from(document.querySelectorAll(`#${to.params.feature_id} .movie-item img`))
-                            .findIndex((item) => item.getAttribute('item-id') === to.params.item_id);
+  const items = document.getElementById(to.params.feature_id).getElementsByClassName('movie-item');
 
   const canvas = document.getElementById('listCanvas');
-  const canvas_w = canvas.getBoundingClientRect().width;
-  const targetPosition = {x: 0, y: window.getComputedStyle(canvas).getPropertyValue('--ratio_16_9_forw100per')};
 
-  const items = document.getElementById(to.params.feature_id).getElementsByClassName('movie-item');
+  const targetPosition = {x: 0, y: window.getComputedStyle(canvas).getPropertyValue('--ratio_16_9_forw100per')};
+  const canvas_w = canvas.getBoundingClientRect().width;
+
+  let active_index = 0;
 
   const tempElms = document.createElement('div');
   tempElms.style.position = 'relative';
   tempElms.style.zIndex = 100;
+
+  Array.from( items ).forEach((el, index) => {
+    const originImg = el.querySelector('.item-image img');
+    const origin_x = originImg.getBoundingClientRect().x;
+    const origin_y = originImg.getBoundingClientRect().y;
+    const origin_width = originImg.getBoundingClientRect().width;
+
+    const img = originImg.cloneNode();
+    img.style.position = 'fixed';
+    img.style.left = `${origin_x}px`;
+    img.style.top = `${origin_y}px`;
+    img.style.width = `${origin_width}px`;
+    img.style.transformOrigin = '0% 50%'
+
+    originImg.style.opacity = 0;
+
+    img.setAttribute('to_x', `${targetPosition.x - origin_x}px`);
+    img.setAttribute('to_y', `calc(${targetPosition.y} - ${origin_y}px)`);
+    img.setAttribute('to_scale', canvas_w / origin_width);
+
+    tempElms.insertAdjacentElement('beforeend', img);
+
+    if (img.getAttribute('item-id') === to.params.item_id) {
+      active_index = index;
+      img.style.zIndex = 100;
+    }
+  })
   canvas.insertAdjacentElement('afterbegin', tempElms);
 
-  const animation_el = (img, el_index) => {
-    const img_index = el_index - active_index;
-    const img_clientRect = img.getBoundingClientRect();
+  const imgs = Array.from( tempElms.getElementsByTagName('img') );
+  imgs.forEach((img, index) => {
 
-    const to_x_s1 = `${targetPosition.x - img_clientRect.x}px`;
-    const to_y_s1 = `calc(${targetPosition.y} - ${img_clientRect.y}px)`;
-    const to_scale_s1 = canvas_w / img_clientRect.width;
+    const elIndex = index - active_index;
 
-    const to_y_s2 = `calc(${to_y_s1} + ${img_index * canvas_w * 9 / 16}px)`;
-    const to_rotate_s2 = `${45 * img_index}deg`;
-    const to_scale_s2 = img_index === 0 ? 1 : 0.46;
-    const to_opacity_s2 = img_index === 0 ? 1 : 0.4;
+    const to_rotate = `${45 * elIndex}deg`;
+    const to_scale = img.getAttribute('to_scale');
+    const to_y_step2 = `calc(${img.getAttribute('to_y')} + ${elIndex * canvas_w * 9 / 16}px)`;
+    const to_scale_step2 = elIndex === 0 ? 1 * to_scale : 0.46 * to_scale;
+    const to_opacity = elIndex === 0 ? 1 : 0.4;
 
-    const animation = [
+    const animation_el = [
       {transform: 'translate(0, 0) scale(1)'},
       {
         transform: 
-          `translate(${to_x_s1}, ${to_y_s1})
-          scale(${to_scale_s1})`,
+          `translate(${img.getAttribute('to_x')}, ${img.getAttribute('to_y')})
+          scale(${img.getAttribute('to_scale')})`,
         opacity: 1
       },
       {
         transform:
-          `translate(${to_x_s1}, ${to_y_s2})
-          scale(${to_scale_s2 * to_scale_s1})
-          rotate(${to_rotate_s2})`,
-        opacity: to_opacity_s2
+          `translate(${img.getAttribute('to_x')}, ${to_y_step2})
+          scale(${to_scale_step2})
+          rotate(${to_rotate})`,
+        opacity: to_opacity
       }
     ]
-
-    img.animate(animation, transitionDuration);
-
-  }
-
-  Array.from(items).forEach((el, index) => {
-    const originImg = el.querySelector('.item-image img');
-    const origin_clientRect = originImg.getBoundingClientRect();
-
-    const img = originImg.cloneNode();
-    img.style.position = 'fixed';
-    img.style.left = `${origin_clientRect.x}px`;
-    img.style.top = `${origin_clientRect.y}px`;
-    img.style.width = `${origin_clientRect.width}px`;
-    img.style.transformOrigin = '0% 50%'
-    if (index === active_index) {
-      img.style.zIndex = 100;
-    }
-
-    tempElms.insertAdjacentElement('beforeend', img);
-    originImg.style.opacity = 0;
-
-    animation_el(img, index);
+    const transition = img.animate(animation_el, transitionDuration);
 
     Promise.all(
       tempElms.getAnimations({ subtree: true }).map((animation) => animation.finished),
     ).then((e) => {
-      console.log(e)
       if (e.length >= index) {
         next();
       }
     });
+
   })
 
-  canvas.getElementsByClassName('list-wrapper')[0].animate([
+  const animation_back = [
     {
       transform: 'rotateY(0)',
       opacity: 1
@@ -105,10 +107,13 @@ const transition_toPlayer = (to, from, next) => {
       transform: 'rotateY(90deg)',
       opacity: 0
     }
-  ], {
+  ]
+
+  canvas.getElementsByClassName('list-wrapper')[0].animate(animation_back, {
     duration: transitionDuration * 0.4,
     fill: 'forwards'
   })
+
 
 }
 </script>
